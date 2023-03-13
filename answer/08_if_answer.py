bf80525b95e178060e90ed74390dc2556fe3e931
guro-Ishiguro <ryu32349581@gmail.com>
2023-03-13 16:33:41 +0900

    ソート機能修正

--- answer/08_if_answer.py
+++ answer/08_if_answer.py
@@ -4,29 +4,21 @@ num_1 = int(input("1つ目の数字を入力してください"))
 num_2 = int(input("2つ目の数字を入力してください"))
 num_3 = int(input("3つ目の数字を入力してください"))
 
-# 解答1
 # 両端で比較
 if num_1 < num_3:
-    num_1, num_3 = num_3, num_1
+    temp = num_1
+    num_1 = num_3
+    num_3 = temp
 
-# 真ん中と両端を比較
 if num_1 < num_2:
-    num_1, num_2 = num_2, num_1
-elif num_2 < num_3:
-    num_2, num_3 = num_3, num_2
+    temp = num_1
+    num_1 = num_2
+    num_2 = temp
 
-print(num_1)
-print(num_2)
-print(num_3)
-
-# 解答2
-# 先頭から比較
-if num_1 < num_2:
-    num_1, num_2 = num_2, num_1
 if num_2 < num_3:
-    num_2, num_3 = num_3, num_2
-if num_1 < num_2:
-    num_1, num_2 = num_2, num_1
+    temp = num_2
+    num_2 = num_3
+    num_3 = temp
 
 print(num_1)
 print(num_2)
